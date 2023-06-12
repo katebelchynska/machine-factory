15c858fd1ff743d3df6ce75dc867d0f5042d3729
katebelchynska <kateryna.bel@forge.solutions>
2023-06-12 16:58:58 +0300

    FS-004: add code linter to the project

--- src/modules/404/404.tsx
+++ src/modules/404/404.tsx
@@ -1,12 +1,13 @@
-import { Box, Container, Link } from "@mui/material";
-import { PATH } from "../core/constants/navigation";
+/* eslint-disable react/no-unescaped-entities */
+import { Box, Container, Link } from '@mui/material';
+import { PATH } from '../core/constants/navigation';
 
 const Page404 = () => (
   <Box
     sx={{
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
     }}
   >
     <Container maxWidth="xl">
@@ -14,11 +15,7 @@ const Page404 = () => (
         <h1 data-testId="404-heading">Page doesn't exist</h1>
       </Box>
       <Box>
-        <Link
-          data-testId="to-main-page"
-          href={PATH.MAIN_PAGE}
-          underline="hover"
-        >
+        <Link data-testId="to-main-page" href={PATH.MAIN_PAGE} underline="hover">
           Back to the main page
         </Link>
       </Box>
