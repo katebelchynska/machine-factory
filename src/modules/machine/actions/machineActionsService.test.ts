d5a5f13c53f48140e20cd00ea727977072e67eb2
katebelchynska <kateryna.bel@forge.solutions>
2023-06-15 12:23:52 +0300

    FS-015: deleted duplicated tests

--- src/modules/machine/actions/machineActionsService.test.ts
+++ src/modules/machine/actions/machineActionsService.test.ts
@@ -9,37 +9,14 @@ describe('machineActionsService', () => {
     expect(result).toEqual(list);
   });
 
-  it('getList should resolve after a delay', async () => {
-    const list: Action[] = initialActionsState.machineActions;
-    const promise = machineActionsService.getList();
-
-    const result = await promise;
-    expect(result).toEqual(list);
-  });
-
   it('addMachineAction should add a new action', async () => {
     const action: Action = initialActionsState.machineActions[0];
     const result = await machineActionsService.addMachineAction(action);
     expect(result).toEqual(action);
   });
 
-  it('addMachineAction should resolve after a delay', async () => {
-    const action: Action = initialActionsState.machineActions[0];
-    const promise = machineActionsService.addMachineAction(action);
-
-    const result = await promise;
-    expect(result).toEqual(action);
-  });
-
   it('deleteMachineAction should delete an action', async () => {
     const result = await machineActionsService.deleteMachineAction();
-    expect(result).toEqual([]);
-  });
-
-  it('deleteMachineAction should resolve after a delay', async () => {
-    const promise = machineActionsService.deleteMachineAction();
-
-    const result = await promise;
     expect(result).toEqual([]);
   });
 });
--- src/modules/machine/events/machineEventsService.test.ts
+++ src/modules/machine/events/machineEventsService.test.ts
@@ -9,37 +9,14 @@ describe('machineEventsService', () => {
     expect(result).toEqual(list);
   });
 
-  it('getList should resolve after a delay', async () => {
-    const list: Event[] = initialEventsState.machineEvents;
-    const promise = machineEventsService.getList();
-
-    const result = await promise;
-    expect(result).toEqual(list);
-  });
-
   it('addMachineEvent should add a new event', async () => {
     const action: Event = initialEventsState.machineEvents[0];
     const result = await machineEventsService.addMachineEvent(action);
     expect(result).toEqual(action);
   });
 
-  it('addMachineEvent should resolve after a delay', async () => {
-    const action: Event = initialEventsState.machineEvents[0];
-    const promise = machineEventsService.addMachineEvent(action);
-
-    const result = await promise;
-    expect(result).toEqual(action);
-  });
-
   it('deleteMachineEvent should delete an event', async () => {
     const result = await machineEventsService.deleteMachineEvent();
-    expect(result).toEqual([]);
-  });
-
-  it('deleteMachineEvent should resolve after a delay', async () => {
-    const promise = machineEventsService.deleteMachineEvent();
-
-    const result = await promise;
     expect(result).toEqual([]);
   });
 });
